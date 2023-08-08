1902857b838fa33be4057811b14356d2f7c81186
SahilGangurde814 <126548151+SahilGangurde814@users.noreply.github.com>
2023-08-08 11:23:46 +0530

    addedUI
    added pause menu, game won menu
    and added pause after pressing esc.

---
+++ Assets/Scripts/gameWonUI.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Build.Content;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class gameWonUI : MonoBehaviour
+{
+    public GameObject pauseUiCanvas;
+    private bool isPaused = false;
+
+
+    private void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pause();
+        }
+    }
+
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void pause()
+    {
+        isPaused = !isPaused;
+
+        if(isPaused)
+        {
+            pauseUiCanvas.SetActive(true);
+            Time.timeScale = 0f;
+        }
+        else
+        {
+            pauseUiCanvas.SetActive(false);
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    public void Resume()
+    {
+        if(isPaused)
+        {
+            pauseUiCanvas.SetActive(false );
+            Time.timeScale = 1.0f;
+        }
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+
+
+}
+    
